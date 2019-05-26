d042ce9d2cb787e65175b444be57ba8e47b2b9f7
Pithana Monisha Anila <pithanamonisha.anila2016@vitstudent.ac.in>
2019-05-26 14:09:46 +0530

    Add files via upload

---
+++ PLOT1.R
@@ -0,0 +1,13 @@
+source("Sourcecode.R")
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
+
+png(filename='PLOT1.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+
+dev.off()
---
+++ PLOT2.R
@@ -0,0 +1,14 @@
+source("Sourcecode.R")
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+                , .SDcols = c("Emissions")
+                , by = year]
+
+png(filename='PLOT2.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+
+dev.off()
